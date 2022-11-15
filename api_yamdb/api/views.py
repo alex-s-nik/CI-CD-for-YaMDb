e4037836ce639a1d85284bb6ecd0a215fad222d3
alex-s-nik <to_ctrl@mail.ru>
2022-11-15 11:47:37 +0300

    feat: add ready project to initial framework

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from reviews.models import Category, Genre, Review, Title
+from users.permissions import (AuthenticatedPrivilegedUsersOrReadOnly,
+                               ListOrAdminModeratOnly)
+
+from .filters import TitleFilter
+from .serializers import (CategorySerializer, CommentSerializer,
+                          GenreSerializer, ReviewSerializer,
+                          TitleCreateSerializer, TitleSerializer)
+
+
+class CreateListDestroyViewSet(mixins.CreateModelMixin,
+                               mixins.ListModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet,):
+    pass
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    pagination_class = LimitOffsetPagination
+    permission_classes = (AuthenticatedPrivilegedUsersOrReadOnly,)
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pagination_class = LimitOffsetPagination
+    permission_classes = (AuthenticatedPrivilegedUsersOrReadOnly,)
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        serializer.save(author=self.request.user, title_id=title_id)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        return Review.objects.filter(title=title_id)
+
+
+class GenreViewSet(CreateListDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (ListOrAdminModeratOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class CategoryViewSet(CreateListDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+    permission_classes = (ListOrAdminModeratOnly,)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    pagination_class = LimitOffsetPagination
+    filter_class = filterset_class = TitleFilter
+    permission_classes = (ListOrAdminModeratOnly,)
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH',):
+            return TitleCreateSerializer
+        return TitleSerializer
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,167 @@
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from .validators import max_value_current_year
+
+
+class Category(models.Model):
+    """Категории произведений."""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name='Слаг категории'
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Сатегория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    """Жанры произведений."""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг жанра'
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    """Произведение искусства."""
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название произведения'
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год создания',
+        validators=[
+            MinValueValidator(0),
+            max_value_current_year]
+    )
+    description = models.TextField(
+        verbose_name='Описание произведения',
+        blank=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreConnect',
+        related_name='genre',
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='category',
+        verbose_name='Категория',
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        to=Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    score = models.SmallIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+        verbose_name='Оценка',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Время добавления',
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f'Оценка {self.author.username} на {self.title.name}'
+
+    class Meta:
+        verbose_name = 'Обзор'
+        verbose_name_plural = 'Обзоры'
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author'),
+                name='unique_review'
+            )
+        ]
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    review = models.ForeignKey(
+        to=Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Ревью'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Время добавления',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Оценка {self.author.username} на {self.review.title.name}'
+
+
+class GenreConnect(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.genre
